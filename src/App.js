4adbe11f9a5b6fee8ead9fdb54563b455e562406
andrew <andrewtclarkson@gmail.com>
2022-05-18 13:41:32 -0400

    change name and button colour

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import * as React from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
-    <div className="mainContainer">
+    <div className='mainContainer'>
+      <div className='dataContainer'>
+        <div className='header'>👋 Hello friend!</div>
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
+        <div className='bio'>
+          I'm Andrew and I'm learning about Smart Contracts with Solidity.
+          Connect your Ethereum wallet and wave at me!
         </div>
 
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
+        <button className='waveButton' onClick={wave}>
           Wave at Me
         </button>
       </div>
